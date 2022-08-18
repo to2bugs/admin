5645e46f30f5e20283e9e8256907b01a4e375b59
root <root@localhost.localdomain>
2022-08-18 15:14:53 +0800

    feat: 个人中心模块布局

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>profile{{ $t('msg.test') }}</div>
+  <div class="my-container">
+    <el-row>
+      <el-col :span="6">
+        <project-card class="project-card"></project-card>
+      </el-col>
+      <el-col :span="18">
+        <el-card>
+          <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
+              <feature></feature>
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
+              <chapter></chapter>
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.author')" name="author">
+              <author></author>
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import ProjectCard from './components/ProjectCard.vue'
+import Feature from './components/Feature.vue'
+import Chapter from './components/Chapter.vue'
+import Author from './components/Author.vue'
+import { ref } from 'vue'
+const activeName = ref('feature')
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.project-card {
+  margin-right: 20px;
+}
+</style>
